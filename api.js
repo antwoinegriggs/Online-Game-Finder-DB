2b38d489e21bbce8ebec79f2bdaf1d8abd23d8c4
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-28 08:52:35 -0400

    update api.js

--- api.js
+++ api.js
@@ -1,7 +1,24 @@
 // api.js
 
 exports.handler = async (event, context) => {
-  if (event.httpMethod === "POST") {
+  if (event.httpMethod === "GET") {
+    try {
+      // Process the GET request as needed
+      const data = require("./db.json");
+
+      // Return the data as the response
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } catch (error) {
+      // Return an error response if there was an issue processing the request
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Failed to process GET request" }),
+      };
+    }
+  } else if (event.httpMethod === "POST") {
     try {
       // Parse the incoming JSON payload from the request body
       const requestBody = JSON.parse(event.body);
@@ -24,7 +41,7 @@ exports.handler = async (event, context) => {
       };
     }
   } else {
-    // Handle other HTTP methods (e.g., GET, PUT, DELETE) if needed
+    // Handle other HTTP methods (e.g., PUT, DELETE) if needed
     return {
       statusCode: 405,
       body: JSON.stringify({ error: "Method not allowed" }),
